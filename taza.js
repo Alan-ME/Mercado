39746abfee185190a7415831c2e52dca8b8f60d3
Iván Ismael <xtreayt@gmail.com>
2025-05-07 20:16:24 -0300

    Add files via upload

---
+++ taza.js
@@ -0,0 +1,8 @@
+function abrirMenu() {
+    document.getElementById("sidebar").classList.add("visible");
+  }
+  
+  function cerrarMenu() {
+    document.getElementById("sidebar").classList.remove("visible");
+  }
+  
