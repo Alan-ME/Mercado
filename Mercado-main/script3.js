524d2a29403f0e11d7f91e625dbae46dfb915811
Iván Ismael <xtreayt@gmail.com>
2025-05-31 20:40:04 -0300

    Delete Mercado-main directory

--- Mercado-main/script3.js
+++
@@ -1,9 +0,0 @@
-
-  function abrirMenu() {
-    document.getElementById('sidebar').classList.add('abierto');
-  }
-
-  function cerrarMenu() {
-    document.getElementById('sidebar').classList.remove('abierto');
-  }
-
