580980220108a39a62c115fc340c99e1459cb4bd
Iván Ismael Cardozo <cardozoivanismael@gmail.com>
2026-09-17 18:13:13 -0300

    Delete Taza final directory

--- Taza final/javascript/menu-hamburguesa.js
+++
@@ -1,8 +0,0 @@
-function abrirMenu() {
-    document.getElementById("sidebar").classList.add("visible");
-  }
-  
-  function cerrarMenu() {
-    document.getElementById("sidebar").classList.remove("visible");
-  }
-  
